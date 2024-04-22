2123e135332770adeabd868559ecda0d38749a95
sanarehan123 <160534757+sanarehan123@users.noreply.github.com>
2024-04-22 16:16:00 +0500

    Add files via upload
    This is my final corrected game

---
+++ main.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//1) Computer will generate a random number 
+//2) User input for guessing number 
+//3) Compare user input with computer and show results 
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 -10: ",
+    }
+]);
+console.log(answers);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed the right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
